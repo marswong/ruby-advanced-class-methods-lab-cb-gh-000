38156fbc89d66a5837e0a538d29040013db868c5
Mars Wong <marswong618@gmail.com>
2018-01-30 05:25:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,17 @@ class Song
     self.class.all << self
   end
 
+  def initialize(name)
+    @name = name    
+  end
+
   def self.create
     song = self.class.new
     song.save
     song
+  end
+
+  def self.new_by_name(name)
+    song = self.class.new(name)
   end
 end
